bf2834b0ddec2a37690b97b3e398878b041a1bff
Mecca Parker <Mecca@mjp.local>
2018-04-27 19:28:13 -0400

    first commit

---
+++ accelerometer/accel.ino
@@ -0,0 +1,180 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM9DS0.h>
+#include <math.h>
+
+/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
+   which provides a common 'type' for sensor data and some helper functions.
+   
+   To use this driver you will also need to download the Adafruit_Sensor
+   library and include it in your libraries folder.
+
+   You should also assign a unique ID to this sensor for use with
+   the Adafruit Sensor API so that you can identify this particular
+   sensor in any data logs, etc.  To assign a unique ID, simply
+   provide an appropriate value in the constructor below (12345
+   is used by default in this example).
+   
+   Connections (For default I2C)
+   ===========
+   Connect SCL to analog 5
+   Connect SDA to analog 4
+   Connect VDD to 5V DC
+   Connect GROUND to common ground
+
+   History
+   =======
+   2014/JULY/25  - First version (KTOWN)
+*/
+   
+/* Assign a unique base ID for this sensor */   
+Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(1000);  // Use I2C, ID #1000
+
+
+/* Or, use Hardware SPI:
+  SCK -> SPI CLK
+  SDA -> SPI MOSI
+  G_SDO + XM_SDO -> tied together to SPI MISO
+  then select any two pins for the two CS lines:
+*/
+
+#define LSM9DS0_XM_CS 10
+#define LSM9DS0_GYRO_CS 9
+//Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(LSM9DS0_XM_CS, LSM9DS0_GYRO_CS, 1000);
+
+/* Or, use Software SPI:
+  G_SDO + XM_SDO -> tied together to the MISO pin!
+  then select any pins for the SPI lines, and the two CS pins above
+*/
+
+#define LSM9DS0_SCLK 13
+#define LSM9DS0_MISO 12
+#define LSM9DS0_MOSI 11
+
+//Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(LSM9DS0_SCLK, LSM9DS0_MISO, LSM9DS0_MOSI, LSM9DS0_XM_CS, LSM9DS0_GYRO_CS, 1000);
+
+/* Direction Matrix */
+
+#define N 1
+#define NE 2
+#define E 3
+#define SE 4
+#define S 5
+#define SW 6
+#define W 7
+#define NW 8
+
+/**************************************************************************/
+/*
+    Displays some basic information on this sensor from the unified
+    sensor API sensor_t type (see Adafruit_Sensor for more information)
+*/
+/**************************************************************************/
+void displaySensorDetails(void)
+{
+  sensor_t accel, mag, gyro, temp;
+  
+  lsm.getSensor(&accel, &mag, &gyro, &temp);
+  
+  Serial.println(F("------------------------------------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(accel.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(accel.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(accel.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(accel.max_value); Serial.println(F(" m/s^2"));
+  Serial.print  (F("Min Value:    ")); Serial.print(accel.min_value); Serial.println(F(" m/s^2"));
+  Serial.print  (F("Resolution:   ")); Serial.print(accel.resolution); Serial.println(F(" m/s^2"));  
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+  
+  delay(500);
+}
+
+/**************************************************************************/
+/*
+    Configures the gain and integration time for the TSL2561
+*/
+/**************************************************************************/
+void configureSensor(void)
+{
+  // 1.) Set the accelerometer range
+  lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_2G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_4G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_6G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_8G);
+  //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_16G);
+  
+}
+
+/**************************************************************************/
+/*
+    Arduino setup function (automatically called at startup)
+*/
+/**************************************************************************/
+void setup(void) {
+#ifndef ESP8266
+  while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
+#endif
+  Serial.begin(9600);
+  Serial.println(F("LSM9DS0 9DOF Sensor Test")); Serial.println("");
+  
+  /* Initialise the sensor */
+  if(!lsm.begin()) {
+    Serial.print("brooo");
+    /* There was a problem detecting the LSM9DS0 ... check your connections */
+    Serial.print(F("Ooops, no LSM9DS0 detected ... Check your wiring or I2C ADDR!"));
+    while(!lsm.begin());
+  }
+  Serial.println(F("Found LSM9DS0 9DOF"));
+  
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+  
+  /* Setup the sensor gain and integration time */
+  configureSensor();
+  
+  /* We're ready to go! */
+  Serial.println("");
+}
+
+/**************************************************************************/
+/*
+    Arduino loop function, called once 'setup' is complete (your own code
+    should go here)
+*/
+/**************************************************************************/
+int map_angle(float angle) {
+  Serial.println(angle);
+  if (0.0 <= angle && angle < 45.0) return NE;
+  else if (45.0 <= angle && angle < 90.0) return N;
+  else if (90.0 <= angle && angle < 135.0) return NW;
+  else if (135.0 <= angle && angle < 180.0) return W;
+  else if (180.0 <= angle && angle < 225.0) return SW;
+  else if (225.0 <= angle && angle < 270.0) return S;
+  else if (270.0 <= angle && angle < 315.0) return SE;
+  else return E;
+}
+
+void loop(void) {  
+  /* Get a new sensor event */ 
+  sensors_event_t accel, mag, gyro, temp;
+
+  lsm.getEvent(&accel, &mag, &gyro, &temp); 
+  float x_accel = -accel.acceleration.x;
+  float y_accel = -accel.acceleration.y;
+  float z_accel = -accel.acceleration.z;
+
+  float angle = atan2(x_accel, y_accel) * 180.0/M_PI;
+  int dir = map_angle(angle);
+
+  Serial.print("x: ");
+  Serial.print(x_accel);
+  Serial.print("  y: ");
+  Serial.print(y_accel);
+  Serial.print("  Direction: ");
+  Serial.println(dir);
+
+  Serial.println("**********************\n");
+
+  delay(500);
+}
